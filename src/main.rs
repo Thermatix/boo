09826b7337eb527aa9ceabca7cd4d371b21918e6
Martin Becker <mbeckerwork@gmail.com>
2019-07-03 22:21:43 +0100

    Adds app loop

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,37 @@ use std::io;
 use tui::Terminal;
 use tui::backend::CrosstermBackend as Backend;
 
+use crossterm::{input, RawScreen};
+
 mod interface;
+mod events;
 
 fn main() -> Result<(), io::Error> {
     let backend = Backend::new();
     let mut terminal = Terminal::new(backend)?;
-    terminal.draw(|mut frame| interface::draw_ui(&mut frame) )
+    if let Ok(_raw) = RawScreen::into_raw_mode() {
+        let input = input();
+        let mut stdin = input.read_sync();
+
+        input.enable_mouse_mode().unwrap();
+
+        loop {
+            terminal.draw(|mut frame| interface::draw_ui(&mut frame));
+            if let Some(key_event) = stdin.next() {
+               match events::process_input_event(key_event) {
+                   events::Event::Keyboard(e) => {
+                       match e {
+                           events::KeyboardAction::Close => break (),
+                           _ => (),
+                       };
+                   },
+                   _ => (),
+               }
+            }
+        };
+        input.disable_mouse_mode().unwrap();
+        Result::Ok(())
+    } else {
+        Result::Ok(())
+    }
 }
