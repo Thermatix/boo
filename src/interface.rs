6b8d3a61851de096865b09a9f2ede4693289d271
Martin Becker <mbeckerwork@gmail.com>
2019-07-03 14:09:40 +0100

    Builds interface

--- src/interface.rs
+++ src/interface.rs
@@ -1,52 +1,64 @@
-use tui::layout::Rect as Size;
+use tui::layout::Rect;
 use tui::layout;
 use tui::terminal::Frame as F;
 use tui::widgets;
-use super::Backend;
+use super::Backend as B;
 
-type Frame = F<Backend>;
+// type &F<B> = F<Backend>;
 
 
 
 pub mod elements {
-    use super::Size;
-    use super::Frame;
+    use super::{F, B};
+    use super::Rect;
     use super::widgets::{Widget, Block, Borders};
 
-    pub fn Drawer(mut frame: Frame, constraint: super::Size) {
+    pub fn Drawer(frame: &mut F<B>, constraint: Rect) {
         Block::default()
             .title("Drawer")
             .borders(Borders::ALL)
-            .render(&mut frame, constraint);
+            .render(frame, constraint);
     }
 
-    pub fn ViewPort(mut frame: Frame, constraint: Size) {
+    pub fn ViewPort(frame: &mut F<B>, constraint: Rect) {
         Block::default()
             .title("View Port")
             .borders(Borders::ALL)
-            .render(&mut frame, constraint);
+            .render(frame, constraint);
     }
 }
 
 pub mod layouts {
-    use super::Frame;
+    use super::{F, B, Rect};
     use super::layout::{Layout, Constraint, Direction};
 
-    type LayoutChunks = std::collections::HashMap<String, super::Size>;
+    type LayoutChunks = std::collections::HashMap<String, Rect>;
 
-    pub fn main(frame: &Frame) -> LayoutChunks {
-        make_constraints(&frame, ["left".to_owned(), "right".to_owned()].as_ref(), [20 as u16, 80 as u16].as_ref())
+    pub fn main(frame: &F<B>) -> LayoutChunks {
+        make_constraints(&frame,
+                         ["left".to_string(),"right".to_string()].as_ref(),
+                         [20 as u16, 80 as u16].as_ref(),
+                         Direction::Horizontal)
     }
 
-    fn make_constraints(frame: &Frame, keys: &[String], sizes: &[u16]) -> LayoutChunks {
-        let res = LayoutChunks::new();
-        for (i, constraint) in  Layout::default()
-                                .direction(Direction::Vertical)
-                                .margin(1)
-                                .constraints(sizes.iter().map(|s| Constraint::Percentage(*s)))
-                                .split(frame.size()) {
-            res.insert(keys[i].to_owned(), constraint);
+    fn make_constraints(frame: &F<B>, keys: &[String], sizes: &[u16], direction: Direction) -> LayoutChunks {
+        let mut res = LayoutChunks::new();
+        let mut constraints = Vec::new();
+
+        for s in sizes {
+            constraints.push(Constraint::Percentage(*s))
         }
+
+       for (i, c) in Layout::default()
+            .direction(direction)
+            .margin(1)
+            .constraints(constraints)
+            .split(frame.size()).iter().enumerate() {
+
+            res.insert(keys[i].to_string(), *c);
+        }
+
+        println!("{:?}", res);
         res
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use tui::Terminal;
+use tui::backend::CrosstermBackend as Backend;
+
+mod interface;
+
+fn main() -> Result<(), io::Error> {
+    let backend = Backend::new();
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|mut frame| {
+        let size = frame.size();
+        let main_view = interface::layouts::main(&frame);
+
+        println!("{:?}", main_view);
+
+        interface::elements::Drawer(&mut frame, main_view["left"]);
+        interface::elements::ViewPort(&mut frame, main_view["right"]);
+
+    })
 }
